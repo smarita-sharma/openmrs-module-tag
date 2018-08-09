53496f864723575326f8351d8a4001478d9b3409
smartyrad <smarita.sharma2015@vit.ac.in>
2018-08-09 14:21:52 +0530

    added new table to liquibase file

---
+++ omod/src/main/java/org/openmrs/tag/web/controller/TagController.java
@@ -0,0 +1,26 @@
+package org.openmrs.tag.web.controller;
+
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
+import org.openmrs.tag.api.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/rest/" )
+public class TagController extends BaseRestController {
+	
+	@Autowired
+	private TagService tagService;
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/tag/unique")
+	@ResponseBody
+	public List<String> allTags() {
+		return tagService.getAllTags();
+	}
+}
